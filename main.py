4f58cf085f449505716ee29e7d78df4d4ce0bae0
shawn <shawnhutchins1@gmail.com>
2025-08-17 22:12:43 -0400

    Added PONG title to screen and progress on paddle class

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from turtle import Screen
+from paddle import Paddle
 
 screen = Screen()
 screen.setup(800, 600)
 screen.bgcolor("black")
+screen.title("PONG")
 
-
+paddle = Paddle()
 
 screen.exitonclick()
