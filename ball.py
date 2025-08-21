754d87b16cd0a15f8a55d7c42ee153ba8517b0da
shawn <shawnhutchins1@gmail.com>
2025-08-20 21:54:52 -0400

    Added ball bouncing off top and bottom

--- ball.py
+++ ball.py
@@ -1,5 +1,7 @@
 from turtle import Turtle
 
+BALL_SPEED = 10
+
 class Ball(Turtle):
 
     def __init__(self):
@@ -8,9 +10,14 @@ class Ball(Turtle):
         self.shape("circle")
         self.color("white")
         self.goto(0, 0)
+        self.direction = [1, 1]
 
+    # noinspection SpellCheckingInspection
     def move_ball(self):
-        newx = self.xcor() + 10
-        newy = self.ycor() + 10
-        self.goto(newx, newy)
+        if self.ycor() > 270 or self.ycor() < -270:
+            self.direction[1] *= -1
+        newxcor = self.xcor() + (BALL_SPEED * self.direction[0])
+        newycor = self.ycor() + (BALL_SPEED * self.direction[1])
+        self.goto(newxcor, newycor)
+
 
